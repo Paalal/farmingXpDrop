bf5f6b7c262bf621b0de3915194706f3695bed86
Palalal <devcodedred@gmail.com>
2023-05-18 00:06:08 +0200

    changed farmer resources

--- src/main/java/de/projectethos/farmingxpdrop/BlockBreakListener.java
+++ src/main/java/de/projectethos/farmingxpdrop/BlockBreakListener.java
@@ -5,6 +5,8 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Ageable;
 import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,9 +17,10 @@ import java.util.List;
 import java.util.Random;
 
 public class BlockBreakListener implements Listener {
+
     final Random rand = new Random();
 
-    final List<Material> farmerResources = Arrays.asList(Material.WHEAT, Material.CARROT, Material.BEETROOT, Material.POTATO, Material.MELON, Material.PUMPKIN, Material.SUGAR_CANE);
+    final List<Material> farmerBlocks = Arrays.asList(Material.MELON, Material.PUMPKIN, Material.SUGAR_CANE);
     final List<Material> logs = Arrays.asList(Material.OAK_LOG, Material.DARK_OAK_LOG, Material.SPRUCE_LOG, Material.BIRCH_LOG, Material.ACACIA_LOG, Material.MANGROVE_LOG, Material.JUNGLE_LOG, Material.CRIMSON_STEM, Material.WARPED_STEM);
     final List<Material> leafs = Arrays.asList(Material.OAK_LEAVES, Material.DARK_OAK_LEAVES, Material.SPRUCE_LEAVES, Material.BIRCH_LEAVES, Material.ACACIA_LEAVES, Material.MANGROVE_LEAVES, Material.JUNGLE_LEAVES);
     final List<Material> lowArcheologyResources = Arrays.asList(Material.DIRT, Material.GRASS_BLOCK, Material.SAND, Material.GRAVEL, Material.MYCELIUM, Material.RED_SAND, Material.ROOTED_DIRT);
@@ -26,9 +29,23 @@ public class BlockBreakListener implements Listener {
     @EventHandler
     public void onFarmerResourceBreak(BlockBreakEvent e) {
         Block block = e.getBlock();
-        if (!isFarmerResource(block.getState().getBlockData().getMaterial())) return;
-        if (isPlayerPlaced(block)) return;
-        if (rand.nextInt(89) == 0) dropXP(block, rand.nextInt(2) + 1 );
+        BlockData blockData = block.getBlockData();
+        Material material = blockData.getMaterial();
+
+        if (isFarmerBlock(material)) {
+            if (isPlayerPlaced(block)) return;
+            if (rand.nextInt(19) == 0) dropXP(block, rand.nextInt(2) + 1);
+            return;
+        }
+
+        if (material.equals(Material.WHEAT) || material.equals(Material.CARROT) || material.equals(Material.POTATO)) {
+            if (((Ageable) blockData).getAge() == 7 || rand.nextInt(29) == 0) dropXP(block, rand.nextInt(2) + 1);
+            return;
+        }
+
+        if (material.equals(Material.BEETROOTS)) {
+            if (((Ageable) blockData).getAge() == 3 || rand.nextInt(29) == 0) dropXP(block, rand.nextInt(2) + 1);
+        }
     }
 
     @EventHandler
@@ -63,9 +80,9 @@ public class BlockBreakListener implements Listener {
         if (rand.nextInt(89) == 0) dropXP(block, rand.nextInt(4) + 1 );
     }
 
-    private boolean isFarmerResource(Material material) {
-        for (Material resource : farmerResources) {
-            if (material.equals(resource)) {
+    private boolean isFarmerBlock(Material material) {
+        for (Material farmerBlock : farmerBlocks) {
+            if (material.equals(farmerBlock)) {
                 return true;
             }
         }
@@ -73,8 +90,8 @@ public class BlockBreakListener implements Listener {
     }
 
     private boolean isLog(Material material) {
-        for (Material resource : logs) {
-            if (material.equals(resource)) {
+        for (Material log : logs) {
+            if (material.equals(log)) {
                 return true;
             }
         }
@@ -82,8 +99,8 @@ public class BlockBreakListener implements Listener {
     }
 
     private boolean isLeaf(Material material) {
-        for (Material resource : leafs) {
-            if (material.equals(resource)) {
+        for (Material leaf : leafs) {
+            if (material.equals(leaf)) {
                 return true;
             }
         }
@@ -91,8 +108,8 @@ public class BlockBreakListener implements Listener {
     }
 
     private boolean isLowArcheologyResource(Material material) {
-        for (Material resource : lowArcheologyResources) {
-            if (material.equals(resource)) {
+        for (Material lowArcheologyResource : lowArcheologyResources) {
+            if (material.equals(lowArcheologyResource)) {
                 return true;
             }
         }
@@ -100,8 +117,8 @@ public class BlockBreakListener implements Listener {
     }
 
     private boolean isHighArcheologyResource(Material material) {
-        for (Material resource : highArcheologyResources) {
-            if (material.equals(resource)) {
+        for (Material highArcheologyResource : highArcheologyResources) {
+            if (material.equals(highArcheologyResource)) {
                 return true;
             }
         }
